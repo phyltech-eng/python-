bd1203630906882675f397137bad0ac2f6624fb4
Phylis Kemuma <phyliskemuma6@gmail.com>
2025-05-17 19:08:45 +0300

    some revision

---
+++ binary search/input_f.py
@@ -0,0 +1,9 @@
+#input function 
+ #the syntax for the input function is
+# input(prompt)
+# name= input("Enter your name: ")
+# print("Hello, " + name + "how are you?")
+# vvariable
+name= input("Enter your name: ")
+length= len(name)
+print(length)
---
+++ binary search/jenny.py
@@ -0,0 +1,7 @@
+#string manipulation
+# print("string manipulation excercise")
+# print("1. string concatenation which is done with a  "+" sign")
+# print("eg: 'hello' + 'world' = 'helloworld'")
+# print("new lines can be created witha backslash n")
+
+
