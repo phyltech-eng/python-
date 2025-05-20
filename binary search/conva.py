85026ec7015da2d048e60ebce464d56d9c499cb4
Phylis Kemuma <phyliskemuma6@gmail.com>
2025-05-20 22:49:34 +0300

    m

---
+++ binary search/conva.py
@@ -0,0 +1,8 @@
+kilometer=float(input("Enter value in kilometer: "))
+#conversion factor
+conversion_factor=0.621371
+#conversion
+miles=kilometer*conversion_factor
+print("%0.2f kilometer is equal to %0.2f miles" %(kilometer,miles))
+
+print(f"{kilometer} kilometers is equal to {miles} miles")
