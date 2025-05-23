baefe4bb5a0230f38155dceb2f82e3d9c74385f8
Phylis Kemuma <phyliskemuma6@gmail.com>
2025-05-24 01:00:49 +0300

    mk

--- binary search/fanct.py
+++ binary search/fanct.py
@@ -10,3 +10,13 @@ my_function()
 #function with parameters:this is a function that takes parameters
 def my_function_with_parameters(name):
     print("Hello " + name + " how are you?")
+
+#call the function
+my_function_with_parameters("John")
+ #lambda function: this is a small anonymous function
+#syntax: lambda arguments: expression
+#creating a lambda function
+
+my_lambda_function = lambda x: x + 1
+#calling the lambda function
+print(my_lambda_function(5))
