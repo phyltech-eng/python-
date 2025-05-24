e38d426bef54e121a6e4ff5b9ae01e200fc080e2
Phylis Kemuma <phyliskemuma6@gmail.com>
2025-05-24 23:43:01 +0300

    conditionals

---
+++ binary search/condi.py
@@ -0,0 +1,10 @@
+#if/else condition are used to do something based on something being true or false
+
+x = 10
+y = 20
+
+
+if x < y:
+    print("x is less than y")
+elif x > y:
+    print("x is greater than y")
